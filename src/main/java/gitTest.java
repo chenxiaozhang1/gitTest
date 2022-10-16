882366291140673d1ad103751df9d3335c65afe6
chenxiaozhang <758093236@qq.com>
2022-10-16 11:52:48 +0800

    cxz first

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName gitTest
+ * @Description gitTest
+ * @Author Administrator
+ * @Date 2022/10/16 11:50
+ * @Version 1.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("cxz 1");
+    }
+}
